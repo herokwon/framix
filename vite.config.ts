b2f3dd7f07a50daed45c7bdd1d9593534b166999
herokwon <contact@herokwon.dev>
2025-06-19 01:56:34 +0900

    chore: Configure `Tailwind CSS`

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tailwindcss(), tsconfigPaths()],
   test: {
     globals: true,
     pool: 'threads',
