4326365233dce28e61476386f4efeab1a3ba8160
herokwon <contact@herokwon.dev>
2025-06-18 21:11:11 +0900

    style: Apply `Prettier` formatting

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import react from "@vitejs/plugin-react-swc";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
