4ffd95225c07d41c79ead71a6f54be0a840e407f
herokwon <contact@herokwon.dev>
2025-06-25 23:34:39 +0900

    chore(eslint): Update file patterns for configurations

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,12 @@
-import eslint from '@eslint/js';
 import eslintConfigPrettier from 'eslint-config-prettier/flat';
 import jsxA11y from 'eslint-plugin-jsx-a11y';
 import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import storybook from 'eslint-plugin-storybook';
+
+import eslint from '@eslint/js';
+
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
@@ -13,37 +15,25 @@ export default tseslint.config(
   ...tseslint.configs.recommended,
   { ignores: ['dist', 'node_modules', 'coverage'] },
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ['**/*.config.{js,cjs,mjs,ts}'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+  {
+    files: ['**/*.{js,jsx,mjs,cjs,ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+      },
       parserOptions: {
         ecmaFeatures: {
           jsx: true,
         },
-        projectService: true,
       },
-    },
-    plugins: {
-      'jsx-a11y': jsxA11y,
-      react,
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...jsxA11y.configs.recommended.rules,
-      ...react.configs.recommended.rules,
-      ...react.configs['jsx-runtime'].rules,
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-      // Allow unused arguments that start with _ (e.g., _event) for clarity and future extensibility
-      '@typescript-eslint/no-unused-vars': [
-        'error',
-        { argsIgnorePattern: '^_' },
-      ],
     },
     settings: {
       react: {
@@ -51,6 +41,11 @@ export default tseslint.config(
       },
     },
   },
+  react.configs.flat.recommended,
+  react.configs.flat['jsx-runtime'],
+  reactHooks.configs['recommended-latest'],
+  reactRefresh.configs.vite,
+  jsxA11y.flatConfigs.recommended,
   storybook.configs['flat/recommended'],
   eslintConfigPrettier,
 );
