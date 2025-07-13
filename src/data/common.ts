114a86c780b8e85f8efecc689c4c2e36ba2b43e9
herokwon <contact@herokwon.dev>
2025-07-13 20:51:11 +0900

    feat(data): Add `common` constants for element props

---
+++ src/data/common.ts
@@ -0,0 +1,10 @@
+export const ELEMENT_SIZES = ['sm', 'md', 'lg', 'xl'] as const;
+export const ELEMENT_SPACINGS = ['compact', 'normal', 'loose'] as const;
+export const ELEMENT_APPEARANCES = [
+  'default',
+  'primary',
+  'success',
+  'danger',
+  'warning',
+  'info',
+] as const;
