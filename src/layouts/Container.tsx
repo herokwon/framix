24c21e86649674ae537ce97aeebf5ca6bc3344e4
herokwon <contact@herokwon.dev>
2025-07-13 20:59:44 +0900

    feat(layouts): Add `Container` layout component

---
+++ src/layouts/Container.tsx
@@ -0,0 +1,60 @@
+import type {
+  ElementSize,
+  PolymorphicPropsWithRef,
+  StrictExtract,
+} from '@types';
+
+import { cn } from '@utils';
+
+import { Box } from '@layouts';
+
+type ContainerTags = StrictExtract<
+  React.ElementType,
+  | 'main'
+  | 'section'
+  | 'div'
+  | 'article'
+  | 'aside'
+  | 'header'
+  | 'footer'
+  | 'dialog'
+>;
+type ContainerProps<T extends ContainerTags> = PolymorphicPropsWithRef<
+  T,
+  | {
+      fixed: true;
+    }
+  | {
+      fixed?: false;
+      maxWidth?: ElementSize;
+    }
+>;
+
+const Container = <T extends ContainerTags = 'section'>(
+  props: ContainerProps<T>,
+) => {
+  const { as: Component = 'section', className, ...rest } = props;
+  return (
+    <Box
+      {...rest}
+      as={Component satisfies ContainerTags}
+      className={cn(
+        className,
+        props.fixed
+          ? 'container'
+          : !props.maxWidth
+            ? 'w-full'
+            : (
+                {
+                  sm: 'sm:max-w-[40rem]',
+                  md: 'md:max-w-3xl',
+                  lg: 'lg:max-w-5xl',
+                  xl: 'xl:max-w-7xl',
+                } satisfies { [width in typeof props.maxWidth]: string }
+              )[props.maxWidth],
+      )}
+    />
+  );
+};
+
+export default Container;
