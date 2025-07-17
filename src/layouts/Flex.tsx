10de225ed2c29cf6d864e9af6030216a7f15b49b
herokwon <contact@herokwon.dev>
2025-07-17 13:50:19 +0900

    feat(layouts): Add `Flex` layout component

---
+++ src/layouts/Flex.tsx
@@ -0,0 +1,107 @@
+import { camelCase } from 'es-toolkit';
+
+import type {
+  CSSValueToClassMap,
+  PolymorphicPropsWithRef,
+  StrictExtract,
+} from '@types';
+
+import { cn } from '@utils';
+
+import { Box } from '@layouts';
+
+type FlexProps<T extends React.ElementType> = PolymorphicPropsWithRef<
+  T,
+  {
+    direction?: StrictExtract<
+      React.CSSProperties['flexDirection'],
+      'column' | 'column-reverse' | 'row' | 'row-reverse'
+    >;
+    justifyContent?: StrictExtract<
+      React.CSSProperties['justifyContent'],
+      | 'start'
+      | 'center'
+      | 'end'
+      | 'space-around'
+      | 'space-between'
+      | 'space-evenly'
+    >;
+    alignItems?: StrictExtract<
+      React.CSSProperties['alignItems'],
+      'start' | 'center' | 'end' | 'stretch' | 'baseline'
+    >;
+    wrap?: StrictExtract<
+      React.CSSProperties['flexWrap'],
+      'nowrap' | 'wrap' | 'wrap-reverse'
+    >;
+    gap?: {
+      row?: number;
+      column?: number;
+    };
+  }
+>;
+
+const Flex = <T extends React.ElementType = 'div'>(props: FlexProps<T>) => {
+  const {
+    as: Component = 'div',
+    direction = 'row',
+    justifyContent = 'start',
+    alignItems = 'stretch',
+    wrap = 'nowrap',
+    gap,
+    className,
+    style,
+    ...rest
+  } = props;
+  return (
+    <Box
+      {...rest}
+      as={Component satisfies React.ElementType}
+      style={{
+        columnGap: `calc(var(--spacing) * ${gap?.column ?? 0})`,
+        rowGap: `calc(var(--spacing) * ${gap?.row ?? 0})`,
+        ...style,
+      }}
+      className={cn(
+        className,
+        'flex',
+        (
+          {
+            row: 'flex-row',
+            rowReverse: 'flex-row-reverse',
+            column: 'flex-col',
+            columnReverse: 'flex-col-reverse',
+          } satisfies CSSValueToClassMap<typeof direction>
+        )[camelCase(direction)],
+        (
+          {
+            start: 'justify-start',
+            center: 'justify-center',
+            end: 'justify-end',
+            spaceAround: 'justify-around',
+            spaceBetween: 'justify-between',
+            spaceEvenly: 'justify-evenly',
+          } satisfies CSSValueToClassMap<typeof justifyContent>
+        )[camelCase(justifyContent)],
+        (
+          {
+            start: 'items-start',
+            center: 'items-center',
+            end: 'items-end',
+            stretch: 'items-stretch',
+            baseline: 'items-baseline',
+          } satisfies CSSValueToClassMap<typeof alignItems>
+        )[camelCase(alignItems)],
+        (
+          {
+            nowrap: 'flex-nowrap',
+            wrap: 'flex-wrap',
+            wrapReverse: 'flex-wrap-reverse',
+          } satisfies CSSValueToClassMap<typeof wrap>
+        )[camelCase(wrap)],
+      )}
+    />
+  );
+};
+
+export default Flex;
--- src/layouts/index.ts
+++ src/layouts/index.ts
@@ -1,2 +1,3 @@
 export { default as Box } from './Box';
 export { default as Container } from './Container';
+export { default as Flex } from './Flex';
