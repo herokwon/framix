ee3587a48918e2a19fe5da0190ddd4bfe1eea887
herokwon <contact@herokwon.dev>
2025-07-30 00:45:23 +0900

    fix(Box): Correct function return type

--- src/layouts/Box.tsx
+++ src/layouts/Box.tsx
@@ -2,7 +2,7 @@ import type { PolymorphicPropsWithRef } from '@types';
 
 const Box = <T extends React.ElementType = 'div'>(
   props: PolymorphicPropsWithRef<T>,
-) => {
+): React.ReactElement => {
   const { as: Component = 'div', ...rest } = props;
   return <Component {...rest} />;
 };
