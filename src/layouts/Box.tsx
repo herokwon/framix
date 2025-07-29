01cfae66697264d0f5cd932c3d5b64cc2ce75a77
herokwon <contact@herokwon.dev>
2025-07-30 00:53:12 +0900

    docs(Box): Add `JSDoc` comments

--- src/layouts/Box.tsx
+++ src/layouts/Box.tsx
@@ -1,5 +1,13 @@
 import type { PolymorphicPropsWithRef } from '@types';
 
+/**
+ * A polymorphic box component that can render as any HTML element.
+ *
+ * @example
+ * <Box>Default div</Box>
+ * <Box as="button">Button</Box>
+ * <Box as="a" href="https://example.com">Link</Box>
+ */
 const Box = <T extends React.ElementType = 'div'>(
   props: PolymorphicPropsWithRef<T>,
 ): React.ReactElement => {
