078485d782853d47b15e4f99194470a80f2d151b
herokwon <contact@herokwon.dev>
2025-08-20 13:39:12 +0900

    fix: Remove `xl` from `ELEMENT_SIZES`

--- src/data/common.ts
+++ src/data/common.ts
@@ -1,4 +1,4 @@
-export const ELEMENT_SIZES = ['sm', 'md', 'lg', 'xl'] as const;
+export const ELEMENT_SIZES = ['sm', 'md', 'lg'] as const;
 export const ELEMENT_SPACINGS = ['compact', 'normal', 'loose'] as const;
 export const ELEMENT_APPEARANCES = [
   'default',
