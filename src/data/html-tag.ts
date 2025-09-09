19f1810a72d3a7ff3cf3a8bfd80f8cf28cd29e7c
herokwon <contact@herokwon.dev>
2025-09-09 16:40:20 +0900

    feat(types/html-tag): Add `HeadingHtmlTag` type

--- src/data/html-tag.ts
+++ src/data/html-tag.ts
@@ -1,1 +1,2 @@
+export const HEADING_HTML_TAGS = ['h1', 'h2', 'h3', 'h4'] as const;
 export const TEXT_HTML_TAGS = ['code', 'em', 'p', 'span', 'strong'] as const;
--- src/types/html-tag.ts
+++ src/types/html-tag.ts
@@ -1,3 +1,4 @@
-import type { TEXT_HTML_TAGS } from '@data';
+import type { HEADING_HTML_TAGS, TEXT_HTML_TAGS } from '@data';
 
+export type HeadingHtmlTag = (typeof HEADING_HTML_TAGS)[number];
 export type TextHtmlTag = (typeof TEXT_HTML_TAGS)[number];
