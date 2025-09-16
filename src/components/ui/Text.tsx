8a98f2c20c143c47e4abc0e400a382ae90bd3930
herokwon <contact@herokwon.dev>
2025-09-16 15:18:43 +0900

    refactor(ui/Text): Simplify color conditional logic for better readability

--- src/components/ui/Text.tsx
+++ src/components/ui/Text.tsx
@@ -48,26 +48,23 @@ const Text = <T extends TextHtmlTag = 'span'>({
 
         // color
         (
-          {
-            default: isColorInverted
-              ? 'text-foreground-dark dark:text-foreground-light'
-              : 'text-foreground-light dark:text-foreground-dark',
-            primary: isColorInverted
-              ? 'text-primary-dark dark:text-primary-light'
-              : 'text-primary-light dark:text-primary-dark',
-            success: isColorInverted
-              ? 'text-success-dark dark:text-success-light'
-              : 'text-success-light dark:text-success-dark',
-            danger: isColorInverted
-              ? 'text-danger-dark dark:text-danger-light'
-              : 'text-danger-light dark:text-danger-dark',
-            warning: isColorInverted
-              ? 'text-warning-dark dark:text-warning-light'
-              : 'text-warning-light dark:text-warning-dark',
-            info: isColorInverted
-              ? 'text-info-dark dark:text-info-light'
-              : 'text-info-light dark:text-info-dark',
-          } satisfies Record<ElementColor, string>
+          (isColorInverted
+            ? {
+                default: 'text-foreground-dark dark:text-foreground-light',
+                primary: 'text-primary-dark dark:text-primary-light',
+                success: 'text-success-dark dark:text-success-light',
+                danger: 'text-danger-dark dark:text-danger-light',
+                warning: 'text-warning-dark dark:text-warning-light',
+                info: 'text-info-dark dark:text-info-light',
+              }
+            : {
+                default: 'text-foreground-light dark:text-foreground-dark',
+                primary: 'text-primary-light dark:text-primary-dark',
+                success: 'text-success-light dark:text-success-dark',
+                danger: 'text-danger-light dark:text-danger-dark',
+                warning: 'text-warning-light dark:text-warning-dark',
+                info: 'text-info-light dark:text-info-dark',
+              }) satisfies Record<ElementColor, string>
         )[color],
 
         // font size
