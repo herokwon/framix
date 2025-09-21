7e26ce98e36c813dcb15f0bedfeea16e1bb13e80
HeroKwon <contact@herokwon.dev>
2025-09-22 01:10:09 +0900

    fix(build): Add dedicated client entry `framix/client` (#45)
    * chore(deps): Update `vite` to latest version
    
    * chore(deps): Add `vite-plugin-dts`
    
    * refactor(Button, LinkButton): Move module exports to `client.ts`
    
    * test(client): Add test cases for client module exports
    
    * chore(deps): Remove `tsup`
    
    * chore(tsup): Integrate build to `vite`
    
    * chore(vite): Integrate build from `tsup`

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 /// <reference types="vitest/config" />
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 import react from '@vitejs/plugin-react-swc';
@@ -16,7 +17,63 @@ const dirname =
     : path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    tsconfigPaths(),
+    dts({
+      entryRoot: 'src',
+      outDir: 'dist/types',
+      tsconfigPath: 'tsconfig.app.json',
+      insertTypesEntry: true,
+      copyDtsFiles: true,
+      include: ['src'],
+      exclude: [
+        '**/__tests__',
+        '**/*.stories.tsx',
+        '**/*.test.tsx',
+        '**/*.spec.tsx',
+      ],
+    }),
+  ],
+  build: {
+    cssCodeSplit: true,
+    cssMinify: true,
+    emptyOutDir: true,
+    sourcemap: true,
+    target: 'es2020',
+    lib: {
+      entry: {
+        index: path.resolve(dirname, 'src/index.ts'),
+        client: path.resolve(dirname, 'src/client.ts'),
+      },
+      name: 'Framix',
+      formats: ['es', 'cjs'],
+      fileName: (format, entryName) =>
+        `${entryName}.${format === 'es' ? 'js' : 'cjs'}`,
+    },
+    rollupOptions: {
+      output: {
+        exports: 'named',
+        banner: chunk => (chunk.name === 'client' ? `'use client';` : ''),
+      },
+      external: [
+        /^react(\/.*)?$/,
+        /^react-dom(\/.*)?$/,
+        'lucide-react',
+        'tailwindcss',
+      ],
+    },
+  },
+  esbuild: {
+    jsx: 'automatic',
+    legalComments: 'none',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(dirname, 'src'),
+    },
+  },
   test: {
     globals: true,
     pool: 'threads',
