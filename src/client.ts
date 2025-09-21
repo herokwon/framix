18acef4ce9d0e9f33b3f62a53bbaf94477dc9e58
herokwon <contact@herokwon.dev>
2025-09-22 00:35:07 +0900

    refactor(Button, LinkButton): Move module exports to `client.ts`

---
+++ src/client.ts
@@ -0,0 +1,4 @@
+'use client';
+
+export { default as Button } from '@components/ui/Button';
+export { default as LinkButton } from '@components/ui/LinkButton';
--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,8 +1,6 @@
 export { default as Alert } from './Alert';
-export { default as Button } from './Button';
 export { default as Heading } from './Heading';
 export { default as IconButton } from './IconButton';
-export { default as LinkButton } from './LinkButton';
 export { default as Spinner } from './Spinner';
 export { default as Text } from './Text';
 export { default as Tooltip } from './Tooltip';
