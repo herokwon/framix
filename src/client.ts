56d6edf8b88cae8b5a693457ac1c9dc022da332d
herokwon <contact@herokwon.dev>
2025-09-22 15:41:28 +0900

    feat(form): Add `Checkbox` form component

--- src/client.ts
+++ src/client.ts
@@ -1,4 +1,6 @@
 'use client';
 
+export { default as Checkbox } from '@components/form/Checkbox';
+
 export { default as Button } from '@components/ui/Button';
 export { default as LinkButton } from '@components/ui/LinkButton';
