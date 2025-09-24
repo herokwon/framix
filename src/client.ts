eebb9fba226013d359cdbe7ef27687f72c58dd15
herokwon <contact@herokwon.dev>
2025-09-25 03:02:27 +0900

    feat(form): Add `RadioGroup` form component

--- src/client.ts
+++ src/client.ts
@@ -2,6 +2,7 @@
 
 export { default as Checkbox } from '@components/form/Checkbox';
 export { default as Radio } from '@components/form/Radio';
+export { default as RadioGroup } from '@components/form/RadioGroup';
 
 export { default as Button } from '@components/ui/Button';
 export { default as LinkButton } from '@components/ui/LinkButton';
---
+++ src/components/form/RadioGroup.tsx
@@ -0,0 +1,55 @@
+import { useState } from 'react';
+
+import type { ElementStatusProps, EssentialProps } from '@types';
+
+import { Box } from '@layouts';
+
+import Radio from './Radio';
+import { RadioGroupProvider } from './RadioGroup.context';
+
+type OneOrMany<T> = T | readonly T[];
+
+type RadioGroupProps = EssentialProps<true> & {
+  children?: OneOrMany<React.ReactElement<typeof Radio>>;
+  value?: string;
+  defaultValue?: string;
+  name?: string;
+  onChange?: (value: string) => void;
+} & Pick<ElementStatusProps, 'isDisabled'>;
+
+const RadioGroup = ({
+  children,
+  testId = 'radio-group',
+  value = '',
+  defaultValue = value,
+  name = 'radio-group',
+  onChange,
+  isDisabled = false,
+}: RadioGroupProps) => {
+  const [selectedValue, setSelectedValue] = useState<string>(defaultValue);
+
+  const isControlled = value.length > 0;
+  const currentValue = isControlled ? value : selectedValue;
+
+  const handleChange = (value: string) => {
+    if (!isControlled) setSelectedValue(value);
+    onChange?.(value);
+  };
+
+  return (
+    <RadioGroupProvider
+      value={{
+        value: currentValue,
+        onChange: handleChange,
+        name,
+        isDisabled,
+      }}
+    >
+      <Box testId={testId} className="w-full space-y-2">
+        {children}
+      </Box>
+    </RadioGroupProvider>
+  );
+};
+
+export default RadioGroup;
